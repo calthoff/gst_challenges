e1ea4271b4b24ce134f2ee3c8f4468017b8bb4ff
Cory Althoff <cory@theselftaughtprogrammer.io>
2018-10-26 10:55:16 -0700

    add new challenges

---
+++ challenge_6/fantasy_game.py
@@ -0,0 +1,55 @@
+import random
+
+
+class Player:
+    def __init__(self, name):
+        self.health = 100
+        self.attack = 20
+        self.name = name
+        self.gold = 0
+
+
+class Monster:
+    def __init__(self, health, attack):
+        self. health = health
+        self.attack = attack
+
+
+class Game:
+    def play_game(self):
+        player = Player(input("What is your name?"))
+        while player.health > 0:
+            input("Press t to start another turn")
+            n = random.randint(0, 3)
+            if n == 0:
+                if self.monster_attack(player):
+                    break
+            elif n == 1:
+                self.find_gold(player)
+            else:
+                print("Nothing happened!")
+
+    def monster_attack(self, player):
+        monster = Monster(random.randint(0, 20), random.randint(0, 20))
+        print("{} you are being attacked by a monster!".format(player.name))
+        health = player.health
+        while True:
+            monster.health -= player.attack
+            player.health -= monster.attack
+            if monster.health < 0 or player.health < 0:
+                break
+        if player.health <= 0:
+            print("You died!")
+            print("Game over! You collected {} pieces of gold".format(player.gold))
+            return True
+        else:
+            print("You lost {} health. Your health is now {}.".format(health - player.health, player.health))
+
+    def find_gold(self, player):
+        gold_found = random.randint(0, 100)
+        player.gold += gold_found
+        print("{} you found {} gold. You have {} gold.".format(player.name, gold_found, player.gold))
+
+
+game = Game()
+game.play_game()
